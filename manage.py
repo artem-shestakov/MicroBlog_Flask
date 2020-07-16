92e7ee26d245af885338cc48b6baf3d863c0e344
artem.s.shestakov@yandex.ru <ashestakov@MacBook-Pro-Artem.local>
2020-07-16 15:24:27 +0300

    Add Tag model and relation many-to-many with Post

--- manage.py
+++ manage.py
@@ -1,9 +1,10 @@
-from main import app, db, migrate, manager, User, Post
+from main import app, db, migrate, manager, User, Post, Comment, Tag, tags
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(app=app, db=db, migrate=migrate, manager=manager,User=User, Post=Post)
+    return dict(app=app, db=db, migrate=migrate, manager=manager, User=User, Post=Post, Comment=Comment, Tag=Tag,
+                tags=tags)
 
 
 if __name__ == '__main__':
