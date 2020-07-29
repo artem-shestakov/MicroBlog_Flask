e50d2db4effcad6f5a6fcdcbc14266cc445e2e37
artem.s.shestakov@yandex.ru <ashestakov@MacBook-Pro-Artem.local>
2020-07-30 00:26:34 +0300

    Add 404 handler for aut module. Add comment in auth module code.

--- wedapp/auth/forms.py
+++ wedapp/auth/forms.py
@@ -9,35 +9,42 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     remember_me = BooleanField("Remember me")
 
+    # Override standart method of FlaskForm class
     def validate(self):
+        # Getting result from standart method of FlaskForm class
         check_validate = super(LoginForm, self).validate()
         if not check_validate:
             return False
 
+        # Additional check user login and password
         user = User.query.filter_by(username=self.username.data).first()
         if not user:
-            self.username.errors.append("Invalid username")
+            self.username.errors.append("Invalid username or password")
             return False
 
         if not user.check_password(self.password.data):
-            self.username.errors.append("Invalid password")
+            self.username.errors.append("Invalid username or password")
             return False
 
         return True
 
 
+# Form for registration
 class RegistrationForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired(), Length(max=255)])
     password = PasswordField("Password", validators=[DataRequired(), Length(min=8)])
     confirm_password = PasswordField("Confirm password", validators=[DataRequired(), EqualTo("password")])
     recaptcha = RecaptchaField()
 
+    # Override standart method of FlaskForm class
     def validate(self):
+        # Getting result from standart method of FlaskForm class
         check_validate = super(RegistrationForm, self).validate()
         if not check_validate:
             return False
 
         user = User.query.filter_by(username=self.username.data).first()
+        # Additional check if user login in database
         if user:
             self.username.errors.append("User with this name already exists")
             return False
--- wedapp/auth/models.py
+++ wedapp/auth/models.py
@@ -3,7 +3,7 @@ from flask_login import AnonymousUserMixin
 from . import bcrypt
 
 
-class BlogAnanimus(AnonymousUserMixin):
+class BlogAnonymus(AnonymousUserMixin):
     def __init__(self):
         self.username = "Guest"
 
@@ -18,6 +18,8 @@ class User(db.Model):
 
     # def __init__(self, username):
     #     self.username = username
+
+    # Block standart methods of Flask Login module
     @property
     def is_authenticated(self):
         if isinstance(self, AnonymousUserMixin):
@@ -39,9 +41,11 @@ class User(db.Model):
     def get_id(self):
         return str(self.id)
 
+    # Encrypt user password
     def set_password(self, password):
         self.password = bcrypt.generate_password_hash(password)
 
+    # Check hash from db and from login form
     def check_password(self, password):
         return bcrypt.check_password_hash(self.password, password)
 
