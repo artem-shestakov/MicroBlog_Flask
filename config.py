3532e5f4bb80e4c399cb51e6a1ba8ca6bca61382
artem.s.shestakov@yandex.ru <ashestakov@MacBook-Pro-Artem.local>
2020-07-31 18:30:19 +0300

    Add AuthO autorization with Twitter

--- config.py
+++ config.py
@@ -3,7 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 mysql_pass = os.getenv("MYSQL_ROOT_PASSWORD")
-
+recaptch_public_key = os.getenv("RECAPTCHA_PUBLIC_KEY")
+recaptcha_private_key = os.getenv("RECAPTCHA_PRIVATE_KEY")
+tw_api_key = os.getenv("TWITTER_API_KEY")
+tw_api_secret = os.getenv("TWITTER_API_SECRET")
 
 class Config(object):
     POSTS_PER_PAGE = 10
@@ -19,6 +22,8 @@ class DevConfig(Config):
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = b'\xe5LpK!\xa4\x99\x92G\xd1T\x82\xdfR\x0c\xb6\x95\xbd\x1c\xab\x19\x94\xc87'
-    RECAPTCHA_PUBLIC_KEY = "6Leh3bYZAAAAAIQ94FG-3blimM-PZx6QyQmn8YqK"
-    RECAPTCHA_PRIVATE_KEY = "6Leh3bYZAAAAAG8aAp2GgtJpM6_0q2rsYIo3f7pW"
+    RECAPTCHA_PUBLIC_KEY = f"{recaptch_public_key}"
+    RECAPTCHA_PRIVATE_KEY = f"{recaptcha_private_key}"
     POSTS_PER_PAGE = 10
+    TWITTER_API_KEY = f"{tw_api_key}"
+    TWITTER_API_SECRET = f"{tw_api_secret}"
