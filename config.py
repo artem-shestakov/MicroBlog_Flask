ce88941a94539cad993ff0d99c3c7c877a6d9343
artem.s.shestakov@yandex.ru <ashestakov@MacBook-Pro-Artem.local>
2020-08-01 01:46:44 +0300

    Update docker-compose file for deploing on remote server

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class ProdConfig(Config):
 
 class DevConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:{mysql_pass}@127.0.0.1:3306/web_blog"
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://root:{mysql_pass}@db:3306/web_blog"
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = b'\xe5LpK!\xa4\x99\x92G\xd1T\x82\xdfR\x0c\xb6\x95\xbd\x1c\xab\x19\x94\xc87'
