1213fff3a10766b1e420093f1bdea2cca8700b69
artem.s.shestakov@yandex.ru <ashestakov@MacBook-Pro-Artem.local>
2020-08-18 00:25:03 +0300

    Add CKEditor to admin post view

--- webapp/admin/routes.py
+++ webapp/admin/routes.py
@@ -2,6 +2,7 @@ from flask_admin import BaseView, expose
 from flask_admin.contrib.sqla import ModelView
 from flask_login import login_required
 from webapp.auth import has_role
+from .forms import CKTextArea
 
 
 class StatisticView(BaseView):
@@ -14,3 +15,12 @@ class StatisticView(BaseView):
 
 class DBView(ModelView):
     pass
+
+
+class PostView(DBView):
+    form_overrides = dict(text=CKTextArea)
+    column_searchable_list = ('text', 'title')
+    column_filters = ('publish_date',)
+
+    create_template = 'admin/post_edit.html'
+    edit_template = 'admin/post_edit.html'
