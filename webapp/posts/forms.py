cc0c854ed33996b338a2ee3014e52fac6a40f0e2
artem.s.shestakov@yandex.ru <ashestakov@MacBook-Pro-Artem.local>
2020-08-19 14:46:46 +0300

    Translate app to Rus

--- webapp/posts/forms.py
+++ webapp/posts/forms.py
@@ -1,14 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField
 from wtforms.validators import DataRequired, Length
+from flask_babel import lazy_gettext as _l
 
 
 # Comment Form
 class CommentForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(max=255, message="This field can't be empty")])
-    text = TextAreaField(u'Comment', validators=[DataRequired(message="This field can't be empty")])
+    name = StringField(_l("Name"), validators=[DataRequired(), Length(max=255, message="This field can't be empty")])
+    text = TextAreaField(_l(u"Comment"), validators=[DataRequired(message="This field can't be empty")])
 
 
 class PostForm(FlaskForm):
-    title = StringField("Title", validators=[DataRequired(), Length(max=255)])
-    text = TextAreaField("Content", validators=[DataRequired()])
+    title = StringField(_l("Title"), validators=[DataRequired(), Length(max=255)])
+    text = TextAreaField(_l("Content"), validators=[DataRequired()])
