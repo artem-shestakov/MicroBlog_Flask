64b9065457f2da65ea5936e822e1b360eeba2968
artem.s.shestakov@yandex.ru <ashestakov@MacBook-Pro-Artem.local>
2020-08-25 16:21:20 +0300

    Add simple Selenium UI test

---
+++ tests/run_test_server.py
@@ -0,0 +1,21 @@
+from webapp import create_app, db
+from webapp.auth.models import User, Role
+
+app = create_app('config.TestConfig')
+
+db.app = app
+db.create_all()
+
+user_role = Role("user")
+author_role = Role("author")
+db.session.add(user_role)
+db.session.add(author_role)
+db.session.commit()
+
+test_user = User(email="test@test.com", f_name="test")
+test_user.set_password("test")
+test_user.roles.append(author_role)
+db.session.add(test_user)
+db.session.commit()
+
+app.run()
