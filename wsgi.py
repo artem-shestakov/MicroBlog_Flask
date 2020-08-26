58ddbd2c352a4be4e1466894983c1a3903a036d8
artem.s.shestakov@yandex.ru <ashestakov@MacBook-Pro-Artem.local>
2020-08-26 18:23:39 +0300

    Config docker depliment with uWSGI

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,3 @@ from webapp.cli import register
 env = os.environ.get("WEBAPP_ENV", "dev")
 app = create_app(f"config.{env.capitalize()}Config")
 register(app)
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, ssl_context='adhoc')
-    # app.run(host="0.0.0.0", port=5000, threaded=True)
