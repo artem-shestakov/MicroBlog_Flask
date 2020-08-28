185c405416a2bc04403d5d63487edd2955f7c2a4
artem.s.shestakov@yandex.ru <ashestakov@MacBook-Pro-Artem.local>
2020-08-28 15:00:50 +0300

    Change SERVER_NAME params in config file

--- config.py
+++ config.py
@@ -88,7 +88,7 @@ class DevConfig(Config):
     #         'schedule': crontab(minute="*/1")
     #     },
     # }
-    SERVER_NAME = "0.0.0.0:3000"
+    SERVER_NAME = "0.0.0.0"
     PREFERRED_URL_SCHEME = "https"
     SECRET_KEY = b'\xe5LpK!\xa4\x99\x92G\xd1T\x82\xdfR\x0c\xb6\x95\xbd\x1c\xab\x19\x94\xc87'
     SMTP_FROM = smtp_from
